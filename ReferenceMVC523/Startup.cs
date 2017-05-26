4ffcf8991097e8f9333767a9857bc4918f67c9ad
rodrigotorres79 <rodrigo@zirian.com>
2017-05-26 11:17:56 -0500

    Initial Load

---
+++ ReferenceMVC523/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReferenceMVC523.Startup))]
+namespace ReferenceMVC523
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
